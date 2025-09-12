1f095a53c69a22bf5ed42cc6d62a5ad9df7975f3
paffexe <kamolov607@gmail.com>
2025-09-13 03:24:10 +0500

    test

--- src/pages/layout/components/Footer.tsx
+++ src/pages/layout/components/Footer.tsx
@@ -3,7 +3,8 @@ import { memo } from 'react';
 const Footer = () => {
   return (
     <div className="h-80 bg-slate-900">
-      <h2>Footer</h2>
+      <h2>Footer abdulaziz</h2>
+      <h2>Added for the test</h2>
     </div>
   );
 };
