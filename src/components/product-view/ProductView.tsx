1ce5415feb56d6f53083d83d599c6be94a299c74
mansurjr <ztm007uzb@gmail.com>
2025-09-15 12:56:26 +0500

    Final

--- src/components/product-view/ProductView.tsx
+++ src/components/product-view/ProductView.tsx
@@ -1,35 +1,89 @@
 import { memo, type FC } from "react";
-import { FaStar, FaRegStar, FaStarHalfAlt } from "react-icons/fa";
 import type { IProduct } from "../../types";
+import RightArrow from "../RightArrow";
+import Star from "../Star";
+import UnderlineLink from "../Underline";
+import { FaRegHeart } from "react-icons/fa6";
 
 interface IProps {
-  data: IProduct[] | undefined;
+  data?: IProduct[];
 }
 
-const ProductView: FC<IProps> = (props) => {
-  const { data } = props;
-
+const ProductView: FC<IProps> = ({ data }) => {
   return (
-    <div className="grid grid-cols-4 gap-6">
-      {data?.map((item: IProduct) => (
-        <div key={item.id}>
-          <div>
-            <img src={item.thumbnail} alt="" />
-          </div>
-          <div>
-            <p>{item.rating}</p>
-            <div className="flex">
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStarHalfAlt />
-              <FaRegStar />
+    <section>
+      <div className="container">
+        <div className="flex justify-between items-end mb-8">
+          <h3 className="font-bold text-3xl">New Arrivals</h3>
+          <UnderlineLink>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <span>More Products</span>
+              <RightArrow />
             </div>
-            <h3 className="font-bold">{item.title}</h3>
-          </div>
+          </UnderlineLink>
         </div>
-      ))}
-    </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {data?.map((item) => (
+            <div key={item.id} className="flex flex-col gap-2">
+              <div className="relative w-full bg-[#F3F5F7] group overflow-hidden py-15">
+                <img
+                  src={item.thumbnail}
+                  alt={item.title}
+                  className="w-full h-full object-contain"
+                />
+
+                <button
+                  className="absolute bottom-4 left-1/2 -translate-x-1/2
+             w-[80%] max-w-[230px] py-3 
+             bg-[#141718] text-white rounded-xl
+             opacity-0 group-hover:opacity-100 
+             translate-y-4 group-hover:translate-y-0
+             transition-all duration-300 hover:cursor-pointer">
+                  Add to cart
+                </button>
+
+                <div className="absolute top-4 left-4 flex flex-col items-center gap-2">
+                  <span className="bg-white  font-semibold px-5 py-1">NEW</span>
+                  {item.discountPercentage && (
+                    <span className="bg-[#38CB89] text-white  font-semibold px-5 py-1 rounded">
+                      -{item.discountPercentage.toFixed(0)}%
+                    </span>
+                  )}
+                </div>
+
+                <button
+                  className="absolute top-2 right-2 bg-white p-2 rounded-full shadow text-gray-600 
+                           opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <FaRegHeart size={14} />
+                </button>
+              </div>
+
+              <div className="flex flex-col gap-1 px-1">
+                <Star rating={item.rating ?? 0} />
+                <span className="font-medium text-sm line-clamp-1">
+                  {item.title}
+                </span>
+                <div className="flex items-center gap-2">
+                  <span className="text-lg font-bold">
+                    $
+                    {(
+                      item.price! -
+                      (item.price! * (item.discountPercentage ?? 0)) / 100
+                    ).toFixed(2)}
+                  </span>
+                  <span className="text-lg text-[#6C7275] line-through">
+                    ${item.price}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="w-full h-[3px] bg-[#343839] rounded my-15" />
+      </div>
+    </section>
   );
 };
 
--- src/pages/home/components/SaleUp.tsx
+++ src/pages/home/components/SaleUp.tsx
@@ -1,10 +1,45 @@
-import { memo } from 'react';
+import { memo } from "react";
+import image from "../../../assets/Image Placeholder.png";
+import RightArrow from "../../../components/RightArrow";
+import UnderlineLink from "../../../components/Underline";
 
 const SaleUp = () => {
   return (
-    <div className="SaleUp">
-      <h2>SaleUp</h2>
-    </div>
+    <section className="bg-py">
+      <div className="flex flex-col lg:flex-row gap-18">
+        <div className="w-full lg:w-1/2">
+          <img
+            src={image}
+            alt="imgwe"
+            className="w-full h-auto object-cover"
+          />
+        </div>
+
+        <div className="w-full lg:w-1/2 space-y-4 py-35 self-center text-left">
+          <div className="flex flex-col gap-4">
+            <p className="font-bold text-[16px] leading-[16px] uppercase text-[#377DFF]">
+              SALE UP TO 35% OFF
+            </p>
+            <h4 className="font-medium text-[40px] leading-[44px] tracking-[-0.4px] max-w-113">
+              HUNDREDS of <br />
+              New lower prices!
+            </h4>
+            <p className="max-w-113 font-normal text-xl leading-8">
+              It’s more affordable than ever to give every room in your home a
+              stylish makeover
+            </p>
+          </div>
+          <UnderlineLink>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <span className="text-[16px] leading-[28px] tracking-[-0.4px]">
+                Shop Now
+              </span>
+              <RightArrow />
+            </div>
+          </UnderlineLink>
+        </div>
+      </div>
+    </section>
   );
 };
 
