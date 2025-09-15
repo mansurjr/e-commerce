00f0d283ca0464b7468ca6fca72e8186dbaa2fd1
mansurjr <ztm007uzb@gmail.com>
2025-09-15 13:01:26 +0500

    Final

--- src/pages/home/components/SaleUp.tsx
+++ src/pages/home/components/SaleUp.tsx
@@ -6,32 +6,32 @@ import UnderlineLink from "../../../components/Underline";
 const SaleUp = () => {
   return (
     <section className="bg-py">
-      <div className="flex flex-col lg:flex-row gap-18">
+      <div className="flex lg:flex-row lg:items-center lg:gap-18 mx-auto px-4 md:px-8 lg:px-16">
         <div className="w-full lg:w-1/2">
           <img
             src={image}
             alt="imgwe"
-            className="w-full h-auto object-cover"
+            className="w-full h-[250px] sm:h-[350px] lg:h-full object-cover"
           />
         </div>
 
-        <div className="w-full lg:w-1/2 space-y-4 py-35 self-center text-left">
+        <div className="w-full lg:w-1/2 py-8 lg:py-35 text-left">
           <div className="flex flex-col gap-4">
-            <p className="font-bold text-[16px] leading-[16px] uppercase text-[#377DFF]">
+            <p className="font-bold text-[14px] sm:text-[16px] leading-[16px] uppercase text-[#377DFF]">
               SALE UP TO 35% OFF
             </p>
-            <h4 className="font-medium text-[40px] leading-[44px] tracking-[-0.4px] max-w-113">
+            <h4 className="font-medium text-2xl sm:text-3xl lg:text-[40px] leading-snug lg:leading-[44px] tracking-[-0.4px] max-w-[28rem]">
               HUNDREDS of <br />
               New lower prices!
             </h4>
-            <p className="max-w-113 font-normal text-xl leading-8">
+            <p className="max-w-[28rem] font-normal text-base sm:text-lg lg:text-xl leading-7 lg:leading-8 text-gray-700">
               It’s more affordable than ever to give every room in your home a
               stylish makeover
             </p>
           </div>
           <UnderlineLink>
-            <div className="flex items-center gap-2 cursor-pointer">
-              <span className="text-[16px] leading-[28px] tracking-[-0.4px]">
+            <div className="inline-flex items-center gap-2 cursor-pointer">
+              <span className="text-[14px] sm:text-[16px] leading-[24px] sm:leading-[28px] tracking-[-0.4px]">
                 Shop Now
               </span>
               <RightArrow />
